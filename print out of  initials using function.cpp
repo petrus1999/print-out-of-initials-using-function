3e097521bcda3660a44664c2b6e60e0ac4213c62
petrus <pshai671@gmail.com>
2022-07-12 08:35:43 +0200

    Add project files.

---
+++ print out of  initials using function.cpp
@@ -0,0 +1,32 @@
+// print out of  initials using function.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+void MyFunction(string name, string surname) {
+
+}
+
+int main(){
+    string name, surname;
+
+    cout << "Enter your name and surname" << endl;
+    cin >> name >> surname;
+
+    cout << " Your initials are " << name[0] <<"." << surname[0] << endl;
+
+    return 0;
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
